aa5e9c677e845cc0729ea015893328f42fcb8cd3
tmsebel <msebeletawana@gmail.com>
2025-07-14 23:23:37 +0200

    Updated app

--- health_monitoring/app.py
+++ health_monitoring/app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import seaborn as sns
 
 from data_utils import simulate_health_data, preprocess_data
